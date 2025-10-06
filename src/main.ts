b95ebe41a09c95e6f603f5848e340f025a84776d
Angel <162243536+AngelCasta34@users.noreply.github.com>
2025-10-06 23:23:34 +0000

    added a clickable button step 1 complete removed example image and text

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+const title = document.createElement("h1");
+title.textContent = "Incremental Game CMPM-121";
+
+const button = document.createElement("button");
+button.textContent = "🐝";
+
+document.body.appendChild(title);
+document.body.appendChild(button);
+
+button.addEventListener("click", () => {
+  console.log("Button Clicked!");
+});

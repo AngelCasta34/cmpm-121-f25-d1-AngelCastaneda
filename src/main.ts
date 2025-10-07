d0db7055ca14df3851a50085daad07b988ca5f05
Angel <162243536+AngelCasta34@users.noreply.github.com>
2025-10-07 00:52:36 +0000

    added counter and display number of the time button is clicked step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import "./style.css";
 const title = document.createElement("h1");
 title.textContent = "Incremental Game CMPM-121";
 
+//counter
+let counter: number = 0;
+const counterDisplay = document.createElement("div");
+counterDisplay.textContent = `${counter} honeycombs`;
+
+//button
 const button = document.createElement("button");
 button.textContent = "🐝";
 
+//elements
 document.body.appendChild(title);
+document.body.appendChild(counterDisplay);
 document.body.appendChild(button);
 
 button.addEventListener("click", () => {
-  console.log("Button Clicked!");
+  counter++;
+  counterDisplay.textContent = `${counter} honeycombs`;
+  console.log(`Button clicked! Total: ${counter}`);
 });
